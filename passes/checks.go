bdcbf85f51470181ae0611046b8ba4d48c51be0a
Lucia Lu <qixialu@hotmail.com>
2026-02-03 23:15:12 -0800

    Fix duplications

--- passes/checks.go
+++ passes/checks.go
@@ -6,25 +6,25 @@ import (
 
 // AllChecks contains all Analyzers that report issues
 var AllChecks = []*analysis.Analyzer{
-	// AZBP001Analyzer,
-	// AZBP002Analyzer,
-	// AZBP003Analyzer,
-	// AZBP004Analyzer,
+	AZBP001Analyzer,
+	AZBP002Analyzer,
+	AZBP003Analyzer,
+	AZBP004Analyzer,
 	AZBP005Analyzer,
-	// AZBP006Analyzer,
-	// AZBP007Analyzer,
-	// AZBP008Analyzer,
+	AZBP006Analyzer,
+	AZBP007Analyzer,
+	AZBP008Analyzer,
 
-	// AZSD001Analyzer,
-	// AZSD002Analyzer,
-	// AZSD003Analyzer,
+	AZSD001Analyzer,
+	AZSD002Analyzer,
+	AZSD003Analyzer,
 
-	// AZRN001Analyzer,
+	AZRN001Analyzer,
 
-	// AZRE001Analyzer,
+	AZRE001Analyzer,
 
-	// AZNR001Analyzer,
-	// AZNR002Analyzer,
-	// AZNR003Analyzer,
-	// AZNR004Analyzer,
+	AZNR001Analyzer,
+	AZNR002Analyzer,
+	AZNR003Analyzer,
+	AZNR004Analyzer,
 }
