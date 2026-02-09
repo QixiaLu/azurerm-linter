e40a8f22a50a962619f85641f84bdb14f5657f8f
Lucia Lu <qixialu@hotmail.com>
2026-02-09 15:26:40 +1100

    Fix test

--- passes/testdata/src/azbp011/a.go
+++ passes/testdata/src/azbp011/a.go
@@ -3,33 +3,19 @@ package azbp011
 import (
 	"strings"
 
-	"github.com/hashicorp/go-azure-helpers/lang/pointer"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/compute/2024-03-01/virtualmachines"
 )
 
-type HibernateSupport string
-
-const HibernateSupportDisabled HibernateSupport = "Disabled"
-const HibernateSupportEnabled HibernateSupport = "Enabled"
-
 func badStringCastingComparison() bool {
-	var hibernateSupport *HibernateSupport
-
-	result1 := strings.EqualFold(string(pointer.From(hibernateSupport)), string(HibernateSupportDisabled)) // want `AZBP011`
-	result2 := strings.EqualFold(string(HibernateSupportEnabled), string(HibernateSupportDisabled))        // want `AZBP011`
+	result1 := strings.EqualFold(string(virtualmachines.VirtualMachinePriorityTypesSpot), string(virtualmachines.VirtualMachinePriorityTypesRegular)) // want `AZBP011`
+	result2 := strings.EqualFold(string(virtualmachines.VirtualMachinePriorityTypesLow), string(virtualmachines.VirtualMachinePriorityTypesSpot))     // want `AZBP011`
 
 	return result1 || result2
 }
 
-func goodDirectEnumComparison() bool {
-	var hibernateSupport *HibernateSupport
-
-	result1 := pointer.From(hibernateSupport) == HibernateSupportEnabled
-	return result1
-}
-
 func legitimateStringEqualFold() bool {
-	userInput := "enabled"
+	userInput := "Low"
 
-	result1 := strings.EqualFold(userInput, string(HibernateSupportEnabled))
+	result1 := strings.EqualFold(userInput, string(virtualmachines.VirtualMachinePriorityTypesLow))
 	return result1
 }
